a4e44ecd8a9ed3f81d3880c0109e776ccf30191f
Andrey Izman <izman.dev@gmail.com>
2016-03-18 07:52:19 +0200

    Add Gruntfile tasks

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function(grunt) {
+    "use strict";
+
+    var livereload = {
+        host: 'localhost',
+        port: 35729
+    };
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        build: {
+            all: {
+                dest: "js/emojionearea.js"
+            }
+        },
+        uglify: {
+            all: {
+                files: {
+                    "js/emojionearea.min.js": ["js/emojionearea.js"]
+                },
+                options: {
+                    preserveComments: false,
+                    sourceMap: true,
+                    ASCIIOnly: true,
+                    sourceMapName: "dist/emojionearea.min.map",
+                    report: "min",
+                    beautify: {
+                        "ascii_only": true
+                    },
+                    banner: "/*! EmojioneArea v<%= pkg.version %> | MIT license */",
+                    compress: {
+                        "hoist_funs": false,
+                        loops: false,
+                        unused: false
+                    }
+                }
+            }
+        },
+        sass: {
+            all: {
+                options: {
+                    unixNewlines: true,
+                    compass: true,
+                    lineNumbers: true
+                },
+                files: {
+                    'css/emojionearea.css': 'scss/emojionearea.scss'
+                }
+            },
+        },
+        watch: {
+            admin: {
+                files: [
+                    'scss/**/*.scss'
+                ],
+                tasks: ['sass'],
+                options: {
+                    livereload: livereload
+                }
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadTasks("tasks");
+
+    grunt.registerTask("default", ["build", "uglify", "sass"]);
+};
