8e7aa025dc819d28559a99e72472ebb7554f079c
Andrey Izman <izman.dev@gmail.com>
2016-03-18 21:31:13 +0200

    Add grunt cssmin task

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,6 +48,16 @@ module.exports = function(grunt) {
                 }
             },
         },
+        cssmin: {
+            target: {
+                files: {
+                    'css/emojionearea.min.css': ['css/emojionearea.css']
+                },
+                options: {
+                    sourceMap: false
+                }
+            }
+        },
         watch: {
             admin: {
                 files: [
@@ -62,6 +72,7 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadTasks("tasks");
