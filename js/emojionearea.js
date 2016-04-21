4c1a35254e7ffcf9848acf6251247500ff44af2f
Merlin Mason <info@merlinmason.co.uk>
2016-04-21 18:00:16 +0100

    build

--- js/emojionearea.js
+++ js/emojionearea.js
@@ -3,7 +3,7 @@
  * https://github.com/mervick/emojionearea
  * Copyright Andrey Izman and other contributors
  * Released under the MIT license
- * Date: 2016-04-21T16:50Z
+ * Date: 2016-04-21T16:59Z
  */
 (function(document, window, $) {
     'use strict';
