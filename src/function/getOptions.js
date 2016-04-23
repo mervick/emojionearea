98ba1eb7497ea0a988d6358cc28a536548342058
Andrey Izman <izman.dev@gmail.com>
2016-04-23 18:05:56 +0300

    Fixed: setting filters in options

--- src/function/getOptions.js
+++ src/function/getOptions.js
@@ -5,6 +5,19 @@ define([
 ],
 function($, default_options, emojioneSupportMode) {
     return function(options) {
+        if (options['filters']) {
+            var filters = default_options.filters;
+            $.each(options['filters'], function(filter, data) {
+                if (typeof data !== 'object' || $.isEmptyObject(data)) {
+                    delete filters[filter];
+                    return;
+                }
+                $.each(data, function(key, val) {
+                    filters[filter][key] = val;
+                });
+            });
+            options['filters'] = filters;
+        }
         options = $.extend({}, default_options, options);
 
         if (emojioneSupportMode > 0) {
