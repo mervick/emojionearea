f28fb5abf430021804ced2d0317b1147dbfac2b5
Andrey Izman <izman.dev@gmail.com>
2016-04-25 15:24:18 +0300

    Update

--- src/prototype/showPicker.js
+++ src/prototype/showPicker.js
@@ -3,7 +3,9 @@ define([
 ],
 function(EmojioneArea) {
     EmojioneArea.prototype.showPicker = function () {
-        var self = this;
+        var self = this,
+            render_emojis = self.editor.data('render_emojis');
+        if (render_emojis) render_emojis();
         if (self._sh_timer) {
             window.clearTimeout(self._sh_timer);
         }
@@ -11,5 +13,6 @@ function(EmojioneArea) {
         self._sh_timer =  window.setTimeout(function() {
             self.button.addClass("active");
         }, 50);
+        return self;
     }
 });
