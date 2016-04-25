ae4426c418904456e28505308163643fa701d29a
Andrey Izman <izman.dev@gmail.com>
2016-04-25 14:56:03 +0300

    Remove support for emojione < 2.0.0

---
+++ src/function/div.js
@@ -0,0 +1,15 @@
+define([
+    'jquery',
+    'var/slice',
+    'function/selector',
+    'function/isObject'
+],
+function($, slice, selector, isObject) {
+    return function(prefix) {
+        var parent = $('<div/>', isObject(prefix) ? prefix : {"class" : selector(prefix, true)});
+        $.each(slice.call(arguments).slice(1), function(i, child) {
+            child.appendTo(parent);
+        });
+        return parent;
+    }
+});
