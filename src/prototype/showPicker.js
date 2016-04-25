daa08b87e01a34022b0b15b5b521cab0fa2d4338
Andrey Izman <izman.dev@gmail.com>
2016-04-25 14:55:44 +0300

    Update

---
+++ src/prototype/showPicker.js
@@ -0,0 +1,15 @@
+define([
+    'prototype/var/EmojioneArea'
+],
+function(EmojioneArea) {
+    EmojioneArea.prototype.showPicker = function () {
+        var self = this;
+        if (self._sh_timer) {
+            window.clearTimeout(self._sh_timer);
+        }
+        self.picker.removeClass("hidden");
+        self._sh_timer =  window.setTimeout(function() {
+            self.button.addClass("active");
+        }, 50);
+    }
+});
