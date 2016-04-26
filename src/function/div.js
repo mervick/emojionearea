34a2c3e581ca660d446589838d1cabb886ef41a6
Andrey Izman <izman.dev@gmail.com>
2016-04-26 18:10:25 +0300

    Update

--- src/function/div.js
+++ src/function/div.js
@@ -8,7 +8,10 @@ function($, slice, selector, isObject) {
     return function(prefix) {
         var parent = $('<div/>', isObject(prefix) ? prefix : {"class" : selector(prefix, true)});
         $.each(slice.call(arguments).slice(1), function(i, child) {
-            child.appendTo(parent);
+            if ($.isFunction(child)) {
+                child = child.call();
+            }
+            $(child).appendTo(parent);
         });
         return parent;
     }
