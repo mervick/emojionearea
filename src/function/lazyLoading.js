3eed43207916940609f9d3e5a2574f03234e5d21
Andrey Izman <izman.dev@gmail.com>
2016-04-27 02:32:32 +0300

    Update

---
+++ src/function/lazyLoading.js
@@ -0,0 +1,19 @@
+define([
+    'jquery'
+],
+function($) {
+    return function() {
+        var self = this;
+        if (!self.sprite && self.lasyEmoji[0]) {
+            var pickerTop = self.picker.offset().top,
+                pickerBottom = pickerTop + self.picker.height() + 20;
+            self.lasyEmoji.not(":hidden").each(function() {
+                var e = $(this), top = e.offset().top;
+                if (top > pickerTop && top < pickerBottom) {
+                    e.attr("src", e.data("src")).removeClass("lazy-emoji");
+                }
+            })
+            self.lasyEmoji = self.lasyEmoji.filter(".lazy-emoji");
+        }
+    }
+});
