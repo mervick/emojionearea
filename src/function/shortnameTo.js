1b1e606b144b6d294f89bbe4a7ba5e12b0dbf9d6
Andrey Izman <izman.dev@gmail.com>
2016-04-27 02:33:28 +0300

    Update

--- src/function/shortnameTo.js
+++ src/function/shortnameTo.js
@@ -1,12 +1,18 @@
 define([
+    'jquery',
     'var/emojione',
     'var/emojioneVersion',
     'function/getTemplate'
 ],
-function(emojione, emojioneVersion, getTemplate) {
-    return function(str, template) {
+function($, emojione, emojioneVersion, getTemplate) {
+    return function(str, template, useSprite, replaces) {
         return str.replace(/:?[\w_\-]+:?/g, function(shortname) {
             shortname = ":" + shortname.replace(/:$/,'').replace(/^:/,'') + ":";
+            if ($.isArray(replaces)) {
+                $.each(replaces, function(i, v) {
+                    template = template.replace('{'+i+'}', v[useSprite ? 0 : 1])
+                });
+            }
             if (shortname in emojione.emojioneList) {
                 return getTemplate(template, emojione.emojioneList[shortname][emojione.emojioneList[shortname].length-1], shortname);
             }
