5ce697e9db8a08d2fd017d4f5860e8267d28c8f3
Andrey Izman <izman.dev@gmail.com>
2016-04-27 04:33:09 +0300

    Update

--- src/block/loadEmojione.js
+++ src/block/loadEmojione.js
@@ -9,13 +9,34 @@ define([
 ],
 function(emojione, uniRegexp, emojioneVersion, readyCallbacks, emojioneSupportMode, emojioneReady, isObject) {
     var cdn_base = "https://cdnjs.cloudflare.com/ajax/libs/emojione/";
-    function detectSupportMode() {
-        return isObject(emojione['jsEscapeMap']) ? emojione.cacheBustParam === "?v=1.2.4" ? 2 : 1 : 0;
+    function detectVersion() {
+        var version = emojione.cacheBustParam;
+        if (!isObject(emojione['jsEscapeMap'])) return '1.5.2';
+        if (version === "?v=1.2.4") return '2.0.0';
+        if (version === "?v=2.0.1") return '2.1.0'; // v2.0.1 || v2.1.0
+        if (version === "?v=2.1.1") return '2.1.1';
+        if (version === "?v=2.1.2") return '2.1.2';
+        if (version === "?v=2.1.3") return '2.1.3';
+        if (version === "?v=2.1.4") return '2.1.4';
+        return '2.1.4';
     }
-    if (!emojione || detectSupportMode() === 0) {
+    function getSupportMode(version) {
+        switch (version) {
+            case '1.5.2': return 0;
+            case '2.0.0': return 1;
+            case '2.1.0':
+            case '2.1.1': return 2;
+            case '2.1.2': return 3;
+            case '2.1.3':
+            case '2.1.4':
+            default: return 4;
+        }
+    }
+    if (!emojione || getSupportMode(detectVersion()) < 2) {
         $.getScript(cdn_base + emojioneVersion + "/lib/js/emojione.min.js", function () {
             emojione = window.emojione;
-            emojioneSupportMode = detectSupportMode();
+            emojioneVersion = detectVersion();
+            emojioneSupportMode = getSupportMode(emojioneVersion);
             cdn_base += emojioneVersion + "/assets";
             var sprite = cdn_base +"/sprites/emojione.sprites.css";
             if (document.createStyleSheet) {
@@ -28,8 +49,9 @@ function(emojione, uniRegexp, emojioneVersion, readyCallbacks, emojioneSupportMo
             }
         });
     } else {
-        emojioneSupportMode = detectSupportMode();
-        cdn_base += (emojioneSupportMode > 1 ? '2.0.0' : '2.1.1') + "/assets";
+        emojioneVersion = detectVersion();
+        emojioneSupportMode = getSupportMode(emojioneVersion);
+        cdn_base += emojioneVersion + "/assets";
     }
 
     emojioneReady(function() {
