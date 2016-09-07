fb7b1bca99cc76f1438b1ade6988939d876b0ac0
Andrey Izman <izmanw@gmail.com>
2016-09-07 21:47:29 +0300

    Fix hide/scroll recent on skinnable tabs

--- src/function/updateRecent.js
+++ src/function/updateRecent.js
@@ -2,16 +2,20 @@ define([
     'function/saveSelection',
     'function/pasteHtmlAtCaret',
     'function/shortnameTo',
-    'function/getRecent',
-    'function/attach'
+    'function/getRecent'
 ],
-function(saveSelection, pasteHtmlAtCaret, shortnameTo, getRecent, attach) {
+function(saveSelection, pasteHtmlAtCaret, shortnameTo, getRecent) {
     return function(self) {
         var emojis = getRecent();
         if (!self.recent || self.recent !== emojis) {
             if (emojis.length) {
-                var scrollTop = self.scrollArea.scrollTop(),
-                    height = self.recentCategory.is(":visible") ? self.recentCategory.height() : 0
+                var skinnable = self.scrollArea.is(".skinnable"),
+                    scrollTop, height;
+
+                if (!skinnable) {
+                    scrollTop = self.scrollArea.scrollTop();
+                    height = self.recentCategory.is(":visible") ? self.recentCategory.height() : 0;
+                }
 
                 var items = shortnameTo(emojis, self.emojiBtnTemplate, true).split('|').join('');
                 self.recentCategory.children(".emojibtn").remove();
@@ -21,15 +25,17 @@ function(saveSelection, pasteHtmlAtCaret, shortnameTo, getRecent, attach) {
                 self.recentCategory.children(".emojibtn").on("click", function() {
                     self.trigger("emojibtn.click", $(this));
                 });
-                // attach(self, , { click: "emojibtn.click" });
 
-                self.recentCategory.show();
                 self.recentFilter.show();
 
-                var height2 = self.recentCategory.height();
+                if (!skinnable) {
+                    self.recentCategory.show();
 
-                if (height !== height2) {
-                    self.scrollArea.scrollTop(scrollTop + height2 - height);
+                    var height2 = self.recentCategory.height();
+
+                    if (height !== height2) {
+                        self.scrollArea.scrollTop(scrollTop + height2 - height);
+                    }
                 }
             } else {
                 if (self.recentFilter.hasClass("active")) {
