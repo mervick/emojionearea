06eda8cc2369ef9e0fd5bcc324f6c73b1570aa88
Andre Lacerda da Silva <lasdeparq@gmail.com>
2017-09-04 13:30:00 -0300

    Adding support to emojione 3. Adding new emojis of version 3 to picker.

--- src/function/loadEmojione.js
+++ src/function/loadEmojione.js
@@ -12,16 +12,24 @@ function(emojione, uniRegexp, emojioneVersion, readyCallbacks, emojioneSupportMo
     return function(options) {
 
         function detectVersion(emojione) {
-            var version = emojione.cacheBustParam;
-            if (!isObject(emojione['jsEscapeMap'])) return '1.5.2';
-            if (version === "?v=1.2.4") return '2.0.0';
-            if (version === "?v=2.0.1") return '2.1.0'; // v2.0.1 || v2.1.0
-            if (version === "?v=2.1.1") return '2.1.1';
-            if (version === "?v=2.1.2") return '2.1.2';
-            if (version === "?v=2.1.3") return '2.1.3';
-            if (version === "?v=2.1.4") return '2.1.4';
-            if (version === "?v=2.2.7") return '2.2.7';
-            return '2.2.7';
+            var version;
+            if(emojione.cacheBustParam) {
+                version = emojione.cacheBustParam;
+                if (!isObject(emojione['jsEscapeMap'])) return '1.5.2';
+                if (version === "?v=1.2.4") return '2.0.0';
+                if (version === "?v=2.0.1") return '2.1.0'; // v2.0.1 || v2.1.0
+                if (version === "?v=2.1.1") return '2.1.1';
+                if (version === "?v=2.1.2") return '2.1.2';
+                if (version === "?v=2.1.3") return '2.1.3';
+                if (version === "?v=2.1.4") return '2.1.4';
+                if (version === "?v=2.2.7") return '2.2.7';
+                return '2.2.7';    
+            } else {
+                version = emojione.emojiVersion;
+                if (version === "3.1") return '3.1.0';
+                return '3.1.0'
+            }
+            
         }
 
         function getSupportMode(version) {
@@ -33,49 +41,90 @@ function(emojione, uniRegexp, emojioneVersion, readyCallbacks, emojioneSupportMo
                 case '2.1.2': return 3;
                 case '2.1.3':
                 case '2.1.4':
-                case '2.2.7':
+                case '2.2.7': return 4;
+                case '3.1.0': return 5;
                 default: return 4;
             }
         }
-        options = getOptions(options);
 
-        if (!cdn.isLoading) {
-            if (!emojione || getSupportMode(detectVersion(emojione)) < 2) {
-                cdn.isLoading = true;
-                $.getScript(cdn.defaultBase + emojioneVersion + "/lib/js/emojione.min.js", function () {
-                    emojione = window.emojione;
-                    emojioneVersion = detectVersion(emojione);
-                    emojioneSupportMode = getSupportMode(emojioneVersion);
-                    cdn.base = cdn.defaultBase + emojioneVersion + "/assets";
-                    if (options.sprite) {
-                        var sprite = cdn.base + "/sprites/emojione.sprites.css";
-                        if (document.createStyleSheet) {
-                            document.createStyleSheet(sprite);
-                        } else {
-                            $('<link/>', {rel: 'stylesheet', href: sprite}).appendTo('head');
-                        }
+        function loadEmojione3Lib() {
+            $.getScript(cdn.defaultBase3 + "@" + emojioneVersion.substring(0,emojioneVersion.lastIndexOf(".")) + "/lib/js/emojione.min.js", function () {
+                emojione = window.emojione;
+                emojioneVersion = detectVersion(emojione);
+                emojioneSupportMode = getSupportMode(emojioneVersion);
+                cdn.base = cdn.defaultBase3 + "-assets@" + emojioneVersion;
+                if (options.sprite) {
+                    var sprite = cdn.base + "/sprites/emojione.sprites-" + emojione.emojiSize + ".css";
+                    if (document.createStyleSheet) {
+                        document.createStyleSheet(sprite);
+                    } else {
+                        $('<link/>', {rel: 'stylesheet', href: sprite}).appendTo('head');
                     }
-                    while (readyCallbacks.length) {
-                        readyCallbacks.shift().call();
-                    }
-                    cdn.isLoading = false;
-                });
-            } else {
+                }
+                while (readyCallbacks.length) {
+                    readyCallbacks.shift().call();
+                }
+                cdn.isLoading = false;
+            });
+        }
+
+        function loadEmojione2Lib() {
+             $.getScript(cdn.defaultBase + "/" + emojioneVersion + "/lib/js/emojione.min.js", function () {
+                emojione = window.emojione;
                 emojioneVersion = detectVersion(emojione);
                 emojioneSupportMode = getSupportMode(emojioneVersion);
                 cdn.base = cdn.defaultBase + emojioneVersion + "/assets";
+                if (options.sprite) {
+                    var sprite = cdn.base + "/sprites/emojione.sprites.css";
+                    if (document.createStyleSheet) {
+                        document.createStyleSheet(sprite);
+                    } else {
+                        $('<link/>', {rel: 'stylesheet', href: sprite}).appendTo('head');
+                    }
+                }
+                while (readyCallbacks.length) {
+                    readyCallbacks.shift().call();
+                }
+                cdn.isLoading = false;
+            });
+        }
+
+        options = getOptions(options);
+        if (!cdn.isLoading) {
+            if (!emojione || getSupportMode(detectVersion(emojione)) < 2) {
+                cdn.isLoading = true;
+                if(getSupportMode(emojioneVersion) > 4) {
+                    loadEmojione3Lib();
+                } else {
+                    loadEmojione2Lib();
+                }
+            } else {
+                emojioneVersion = detectVersion(emojione);
+                emojioneSupportMode = getSupportMode(emojioneVersion);
+                if(emojioneSupportMode > 4) {
+                    cdn.base = cdn.defaultBase3 + "-assets@" + emojioneVersion;
+                } else {
+                    cdn.base = cdn.defaultBase + emojioneVersion + "/assets";
+                }
             }
         }
 
         emojioneReady(function() {
+            var emojiSize = "";
+
             if (options.useInternalCDN) {
-                emojione.imagePathPNG = cdn.base + "/png/";
-                emojione.imagePathSVG = cdn.base + "/svg/";
+                if(emojioneSupportMode > 4) emojiSize = emojione.emojiSize + "/";
+                
+                emojione.imagePathPNG = cdn.base + "/png/" + emojiSize;
+                emojione.imagePathSVG = cdn.base + "/svg/" + emojiSize;
                 emojione.imagePathSVGSprites = cdn.base + "/sprites/emojione.sprites.svg";
                 emojione.imageType = options.imageType;
             }
-
-            uniRegexp = new RegExp("<object[^>]*>.*?<\/object>|<span[^>]*>.*?<\/span>|<(?:object|embed|svg|img|div|span|p|a)[^>]*>|(" + emojione.unicodeRegexp + ")", "gi");
+            if(getSupportMode(emojioneVersion) > 4) {
+                uniRegexp = new RegExp("(" + emojione.regUnicode + ")", "gi");
+            } else {
+                uniRegexp = new RegExp("<object[^>]*>.*?<\/object>|<span[^>]*>.*?<\/span>|<(?:object|embed|svg|img|div|span|p|a)[^>]*>|(" + emojione.unicodeRegexp + ")", "gi");
+            }
         });
     };
 });
--- src/var/cdn.js
+++ src/var/cdn.js
@@ -1,6 +1,7 @@
 define(function() {
     return { 
         defaultBase: "https://cdnjs.cloudflare.com/ajax/libs/emojione/",
+        defaultBase3: "https://cdn.jsdelivr.net/npm/emojione",
         base: null,
         isLoading: false
     };
