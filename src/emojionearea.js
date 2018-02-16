1a618b3258257dc184251d6cb305681c5d3c3c15
Lewys Bosley <lbosley@opengenius.com>
2018-02-16 13:24:51 +0000

    add callable destroy function

--- src/emojionearea.js
+++ src/emojionearea.js
@@ -2,6 +2,7 @@ define([
     'jquery',
     'prototype/var/EmojioneArea',
     'function/getDefaultOptions',
+    'function/destroy',
     'function/htmlFromText',
     'var/blankImg',
     'var/emojioneSupportMode',
@@ -17,13 +18,19 @@ define([
     'prototype/enable',
     'prototype/disable'
 ],
-function($, EmojioneArea, getDefaultOptions, htmlFromText, blankImg, emojioneSupportMode, loadEmojione) {
+function($, EmojioneArea, getDefaultOptions, destroy, htmlFromText, blankImg, emojioneSupportMode, loadEmojione) {
     $.fn.emojioneArea = function(options) {
-        return this.each(function() {
-            if (!!this.emojioneArea) return this.emojioneArea;
-            $.data(this, 'emojioneArea', this.emojioneArea = new EmojioneArea($(this), options));
-            return this.emojioneArea;
-        });
+        if (typeof options === 'string') {
+            if (options === 'destroy') {
+                destroy(this.emojioneArea);
+            }
+        } else {
+            return this.each(function() {
+                if (!!this.emojioneArea) return this.emojioneArea;
+                $.data(this, 'emojioneArea', this.emojioneArea = new EmojioneArea($(this), options));
+                return this.emojioneArea;
+            });
+        }
     };
 
     $.fn.emojioneArea.defaults = getDefaultOptions();
